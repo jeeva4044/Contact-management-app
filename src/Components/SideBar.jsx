187bf7c5445551e4701e7b0c888c4bf93d96bd66
Jeeva_G <jeeva.ct20@bitsathy.ac.in>
2024-04-28 14:25:45 +0530

    first commit

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 export default function Sidebar() {
     return (
         <div className="flex border-r-2 bg-gray-600">
-            <div className="flex flex-col h-screen p-3 bg-white-300 shadow w-60 md:w-56 lg:w-48">
+            <div className="flex pt-16 flex-col h-screen p-3 bg-white-300 shadow w-60 md:w-56 lg:w-48">
                 <div className="space-y-3">
                     <div className="flex items-center">
-                        <h2 className="text-xl mt-4 font-bold text-violet-500">Dashboard</h2>
+                        <h2 className="text-3xl mt-4 font-bold text-violet-500">Dashboard</h2>
                     </div>
                     <div className="flex-1">
                         <ul className="pt-2 pb-4 space-y-1 text-sm">
